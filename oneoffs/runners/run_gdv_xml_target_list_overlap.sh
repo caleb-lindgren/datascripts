2ddfea4c1909f8f731d07354c542e90134a55d67
Caleb Lindgren <calebmlindgren@gmail.com>
2025-05-08 17:14:09 -0400

    Script to select targets from 1200 list that were actually consistently gotten

---
+++ oneoffs/runners/run_gdv_xml_target_list_overlap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+python /mnt/cellbio/Gygi\ Lab/caleb/datascripts/oneoffs/gdv_xml_target_list_overlap.py \
+/mnt/cellbio/Gygi\ Lab/caleb/GoDigMeta/TargetLists/250131_SRSIV001G_GoDig2p0_Primed_GoDigTargets_1285.csv \
+/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06092_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR1.gdvxml \
+/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06094_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_A1.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06093_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR2.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06099_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR1.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06100_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR2.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06101_CML_MS1_match-1min_pep-30sec_2PRLib_CloseOutPRLib_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR1.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06102_CML_MS1_match-1min_pep-30sec_2PRLib_CloseOutPRLib_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR2.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06103_CML_MS1_match-1min_pep-30sec_2PRLib_CloseOutPRLib_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_A1.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06104_CML_EOBin_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k-PR1.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06105_CML_EOBin_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k-PR2.gdvxml \
+#/mnt/cellbio/Gygi\ Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06106_CML_EOBin_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k-A1.gdvxml
