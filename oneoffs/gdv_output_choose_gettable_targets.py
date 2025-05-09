2eb483721c53c9934d359dfa05b197a25759c721
Caleb Lindgren <calebmlindgren@gmail.com>
2025-05-09 14:10:38 -0400

    Based on 2025-05-09 priming run library of 1285 targets, choose 300 targets

--- oneoffs/gdv_output_choose_gettable_targets.py
+++ oneoffs/gdv_output_choose_gettable_targets.py
@@ -11,8 +11,10 @@ from lxml import etree
 # Parse the GDV target table CSVs
 gdv_csvs = []
 for file in [
-	"/mnt/clindgren/cellbio/Gygi Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06092_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR1_TargetTable.csv",
-	"/mnt/clindgren/cellbio/Gygi Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06094_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_A1_TargetTable.csv",
+#	"/mnt/cellbio/Gygi Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06092_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_PR1_TargetTable.csv",
+#	"/mnt/cellbio/Gygi Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06094_CML_MS1_match-1min_pep-30sec_2PR-10bins-noNarrow-savePRLib_60min_1200primed_GoDig2p0_SN180_600-1k_A1_TargetTable.csv",
+	"/mnt/cellbio/Gygi Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06386_CML_MS1_match-1min_pep-30sec_1PR-narrow1st-savePRLib_60min_1285primed_SN180_600-1k_PR1_TargetTable.csv",
+	"/mnt/cellbio/Gygi Lab/caleb/GoDigExperiments/MS1Matching/1200primed/ed06388_CML_MS1_match-5min_pep-30sec_1PR20250509LibOnly_60min_1285primed_SN180_600-1k_A1_TargetTable.csv",
 ]:
 	run = re.search(r"_([PA]R?\d)_TargetTable.csv", file).group(1)
 
@@ -41,7 +43,27 @@ df = (
 	.sort("NumIDSuccess_PR1", "TotalSumSN_A1")
 )
 
+# Check overlap
+v1_overlap = (
+	pl.read_csv("/mnt/cellbio/Gygi Lab/caleb/GoDigMeta/TargetLists/20250509_CML_300gettable-from-1285primed.csv")
+	.join(
+		df.with_row_index(name="rank").select("GeneSymbol", "Peptide", "z", "rank"),
+		on=["GeneSymbol", "Peptide", "z"],
+		how="left"
+	)
+	.sort("rank")
+	.with_row_index(name="rank2")
+)
+
 with pl.Config(tbl_rows=-1, tbl_cols=-1, tbl_width_chars=10000, fmt_str_lengths=1000, fmt_table_cell_list_len=100):
+	print(v1_overlap)
 	print(df)
 
-#df.write_csv("/mnt/clindgren/cellbio/Gygi Lab/caleb/GoDigMeta/TargetLists/20250509_CML_300gettable-from-1285primed.csv", quote_style="non_numeric")
+df = (
+	df
+	.sort("NumIDSuccess_PR1", "TotalSumSN_A1")
+	.tail(300)
+	.select("GeneSymbol", "Peptide", "z")
+)
+
+df.write_csv("/mnt/cellbio/Gygi Lab/caleb/GoDigMeta/TargetLists/20250509_CML_300gettableV2-from-1285primed.csv", quote_style="non_numeric")
