2855be20a4b3906efc4b514733052955455521d8
Caleb Lindgren <calebmlindgren@gmail.com>
2025-07-10 19:59:31 +0000

    Make sampling consistent; sample more numbers

--- oneoffs/core_peptide_view_filtered_make_target_lists.py
+++ oneoffs/core_peptide_view_filtered_make_target_lists.py
@@ -3,7 +3,7 @@ import polars as pl
 import re
 
 # Get the list of peptides in our spectral library
-sl_path = "../../Libraries/240126_SRSIII001_qy-4cell-24frac-noFAIMS-withContams/240126_SRSIII001_qy-4cell-24frac-withContam_RENAMED_BestPSM_SpecLib.xml"
+sl_path = "../../GoDigMeta/Libraries/240126_SRSIII001_qy-4cell-24frac-noFAIMS-withContams/240126_SRSIII001_qy-4cell-24frac-withContam_RENAMED_BestPSM_SpecLib.xml"
 
 with open(sl_path, "r") as sl_handle:
 	speclib_str = sl_handle.read()
@@ -30,7 +30,7 @@ sdf = (
 # Unfiltered peptide tables downloaded from peptide view in Core were filtered using the following script:
 # ../general/core_pepsOnly_processSpectralCounts.py
 dfs = []
-for filtered in sorted(glob.glob("filtered_peps/*.tsv")):
+for filtered in sorted(glob.glob("../../GoDigMeta/TargetLists/5cell_15runs/filtered_peps/*.tsv")):
 	search = re.search(r"ed[0-9]{5}", filtered).group()
 	dfs.append(
 		pl.read_csv(filtered, separator="\t")
@@ -58,28 +58,41 @@ df = (
 	.sort(by=["len", "z"], descending=[True, False])
 	.unique(subset="Peptide", keep="first") # Keep only one charge state per peptide. Take the most often found one.
 	.select("GeneSymbol", "Peptide", "z")
+	.sort(by=["GeneSymbol", "Peptide", "z"])
 )
 
 df5k = (
 	df.sample(n=5000, with_replacement=False, seed=0)
 	.sort("GeneSymbol", "Peptide")
 )
-df5k.write_csv("250703_CML_5k-from-5cell15runs-in-qy4cellLib.csv")
+df5k.write_csv("../../GoDigMeta/TargetLists/5cell_15runs/250710_CML_5k-from-5cell15runs-in-qy4cellLib.csv")
 
 df3k = (
 	df5k.sample(n=3000, with_replacement=False, seed=0)
 	.sort("GeneSymbol", "Peptide")
 )
-df3k.write_csv("250703_CML_3k-from-5cell15runs-in-qy4cellLib.csv")
+df3k.write_csv("../../GoDigMeta/TargetLists/5cell_15runs/250710_CML_3k-from-5cell15runs-in-qy4cellLib.csv")
 
-df1500 = (
-	df3k.sample(n=1500, with_replacement=False, seed=0)
+df2500 = (
+	df3k.sample(n=2500, with_replacement=False, seed=0)
 	.sort("GeneSymbol", "Peptide")
 )
-df1500.write_csv("250703_CML_1500-from-5cell15runs-in-qy4cellLib.csv")
+df2500.write_csv("../../GoDigMeta/TargetLists/5cell_15runs/250710_CML_2500-from-5cell15runs-in-qy4cellLib.csv")
+
+df2k = (
+	df2500.sample(n=2000, with_replacement=False, seed=0)
+	.sort("GeneSymbol", "Peptide")
+)
+df2k.write_csv("../../GoDigMeta/TargetLists/5cell_15runs/250710_CML_2k-from-5cell15runs-in-qy4cellLib.csv")
+
+df1500 = (
+	df2k.sample(n=1500, with_replacement=False, seed=0)
+	.sort("GeneSymbol", "Peptide")
+)
+df1500.write_csv("../../GoDigMeta/TargetLists/5cell_15runs/250710_CML_1500-from-5cell15runs-in-qy4cellLib.csv")
 
 df300 = (
 	df1500.sample(n=300, with_replacement=False, seed=0)
 	.sort("GeneSymbol", "Peptide")
 )
-df300.write_csv("250703_CML_300-from-5cell15runs-in-qy4cellLib.csv")
+df300.write_csv("../../GoDigMeta/TargetLists/5cell_15runs/250710_CML_300-from-5cell15runs-in-qy4cellLib.csv")
